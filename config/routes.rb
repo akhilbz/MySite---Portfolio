60aa9f532a96d84aa3c9b87cd698f17ced85f479
akhilbz <akhilesh.bitla@gmail.com>
2017-11-17 23:03:53 +0530

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-  resources :portfolios
+	# except expects an array
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   # get 'pages/home'
 
