75c2322a234ed281b8dc5fb4de3f90562770595b
akhilbz <akhilesh.bitla@gmail.com>
2017-11-17 22:27:04 +0530

    Cofigured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
 
   resources :portfolios
 
-  get 'pages/home'
+  # get 'pages/home'
 
-  get 'pages/about'
+  get 'About Me' , to: 'pages#about'
 
-  get 'pages/contact'
+  get 'Contact' , to: 'pages#contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root to: 'pages#home'
+  # it is a route to home or whatever
+
 end
