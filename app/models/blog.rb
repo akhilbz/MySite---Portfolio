dd654f746c7646ea16354e235b9924bca38f7259
akhilbz <akhilesh.bitla@gmail.com>
2017-11-27 22:19:31 +0530

    Implemented validations for blogs, skills, and portfolio items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,9 @@ class Blog < ApplicationRecord
 	enum status: { draft: 0, published: 1 }
 	  extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates_presence_of :title, :body
+  # this sees for the items that contain the data from above
 end
 
 
